7eb3287472933e83ee8c4e71cba9f8ceca1b0a4c
Reid Moffat <18rem8@queensu.ca>
2021-11-03 17:54:13 -0400

    Create create-branch.sh

---
+++ git/create-branch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Creates a branch off of this branch and pushes it to the remote (includes error checking)
+function create-branch() {
+    if [[ ! -d .git ]]; then
+        echo "Error: you are not in a git repo" &&
+            return
+    fi
+
+    if [ "$#" -ne 1 ]; then
+        echo "Error: must supply one parameter as the new branch name" &&
+            return
+    fi
+
+    git pull &&
+        git branch -d "$1" &&
+        git push origin -u "$1"
+}
