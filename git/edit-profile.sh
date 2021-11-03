9b5f1298952ed37b8d211ada579f2ab5692b2aaa
Reid Moffat <18rem8@queensu.ca>
2021-11-03 17:50:41 -0400

    Update edit-profile.sh

--- git/edit-profile.sh
+++ git/edit-profile.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
+# Opens .bash_profile in vscode to edit. 'nano .bash_profile'
+# works too, but it's not as user friendly
 function edit-profile() {
     cd &&
-    code .bash_profile && # Opens in vscode
+    code .bash_profile &&
     cd -
 }
