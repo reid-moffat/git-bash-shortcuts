9c1e6fba3ccd805560e3a966cbb1f3077a3df01d
Reid Moffat <18rem8@queensu.ca>
2021-11-14 00:01:24 -0500

    Update misc.sh

--- aliases/misc.sh
+++ aliases/misc.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-alias reload="source ~/.bash_profile" # Reloads your bash profile instead of having to restart your shell (note: all scripts inside will be run again)
+alias bash-reload="source ~/.bash_profile" # Reloads your bash profile instead of having to restart your shell (note: all scripts inside will be run again)
