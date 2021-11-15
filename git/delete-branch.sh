30d201c8a2750cfa31a4959040e46937e03a4458
Reid Moffat <18rem8@queensu.ca>
2021-11-14 22:38:56 -0500

    Update delete-branch.sh

--- git/delete-branch.sh
+++ git/delete-branch.sh
@@ -20,13 +20,29 @@ function delete-branch() {
             git checkout main
     fi
 
+    function delete-branch() {
+    if [[ ! -d .git ]]; then
+        echo "Error: you are not in a git repo root" &&
+            return
+    fi
+
+    if [[ "$#" -ne 1 && "$#" -ne 2 ]]; then
+        echo "Error: you must supply the branch name and optionally the remote name (origin by default)" &&
+            return
+    fi
+
+    if [[ $1 == $(git branch --show-current) ]]; then
+        echo "You are tyring to delete the current branch. Switching to main..." &&
+            git checkout main
+    fi
+
+    # Delete local branch
+    git branch -D "$1"
+
     # Delete the remote branch
     if [ "$#" -ne 1 ]; then
         git push "$2" --delete "$1"
     else
         git push origin --delete "$1"
     fi
-
-    # Delete local branch
-    git branch -D "$1"
 }
