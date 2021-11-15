9cfb7bc8a389c38808968f8ec4133304a4492598
Reid Moffat <18rem8@queensu.ca>
2021-11-14 22:53:44 -0500

    Update auto-git-fetch.sh

--- git/auto-git-fetch.sh
+++ git/auto-git-fetch.sh
@@ -3,7 +3,8 @@
 # If this is placed in your bash profile, opening/reloading a bash
 # terminal in a git repo will automatically fetch updates
 if git rev-parse --git-dir >/dev/null 2>&1; then
-    echo "Fetching git updates..." &&
-        git fetch &&
-        echo "Done"
+    echo "Fetching git updates...";
+    if [ -n $(git fetch) ]; then
+        echo "All up to date"
+    fi
 fi
