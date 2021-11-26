e0cc8d2facc0c3d8c289cc872c3d1a8666b220da
Reid Moffat <18rem8@queensu.ca>
2021-11-26 01:06:29 -0500

    Update refresh-branches.sh

--- git/refresh-branches.sh
+++ git/refresh-branches.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-# Refreshes the list of local branches
-# This is really just one command, but it's much clearer as a function
+# Refreshes the local list of branches based on the remote
 function refresh-branches() {
     if [[ ! -d .git ]]; then
         echo "Error: you are not in a git repo root" &&
             return
     fi
 
-    git remote update origin --prune
+    # Refresh the list of remote branches
+    git remote update origin --prune &&
+
+    # This will delete all the local branches not on the remote, so a confirmation is a good idea
+    read -p "Do you wish to delete local branches not on the remote too ('y' to delete)? " response
+    if [[ "$response" == "y" ]]; then
+        git fetch -p &&
+            for branch in $(git branch -vv | grep ': gone]' | awk '{print $1}'); do
+                git branch -D $branch
+            done
+    fi
 }
