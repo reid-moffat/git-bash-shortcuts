591aad5da36261039ea13b6134613ffafebdd333
Reid Moffat <18rem8@queensu.ca>
2022-02-17 00:25:30 -0500

    Update and rename bash-reload.sh to bash-refresh.sh

--- aliases/bash-refresh.sh
+++ aliases/bash-refresh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# Reloads your bash profile instead of having to restart your shell; this can be used if you edit your bash profile while your terminal is open
+# Refreshes your bash profile instead of having to restart your shell; this can be used if you edit your bash profile while your terminal is open
 # Note: all scripts inside will be run again. Also make sure that the path '~/.bash_profile' is correct on your machine
 alias bash-reload="source ~/.bash_profile"
