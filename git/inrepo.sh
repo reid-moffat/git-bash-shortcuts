3e9aa2ad87d67874681a9b50823592b5a62e3252
Reid Moffat <18rem8@queensu.ca>
2022-09-26 23:18:20 -0400

    Update and rename in-git-repo.sh to inrepo.sh

--- git/inrepo.sh
+++ git/inrepo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Lets you know if you ar ecurrently in a git repository (including subdirectories)
-function in-git-repo() {
+function inrepo() {
     if git rev-parse --git-dir >/dev/null 2>&1; then
         echo "You are in git repo"
     else
