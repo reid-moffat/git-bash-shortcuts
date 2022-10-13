680e8b8d53daadd45a5a27893b72d26da6b43a77
Reid Moffat <18rem8@queensu.ca>
2022-10-12 22:28:54 -0400

    rename

---
+++ bash/profile-utilities.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Opens .bash_profile in vscode to edit (must have vscode installed), making editing bash
+alias epro="code ~/.bash_profile"
+
+# Refreshes your bash profile instead of having to restart your shell; this can be used if you edit your bash profile while your terminal is open
+# Note: all scripts inside will be run again. Also make sure that the path '~/.bash_profile' is correct on your machine
+alias brel="source ~/.bash_profile"
+
+
