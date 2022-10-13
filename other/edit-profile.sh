e4f5e7a57c96ce89c0c97c67f640b5a4231f57d1
Reid Moffat <18rem8@queensu.ca>
2022-10-12 21:36:24 -0400

    Update edit-profile.sh

--- other/edit-profile.sh
+++ other/edit-profile.sh
@@ -2,8 +2,4 @@
 
 # Opens .bash_profile in vscode to edit
 # 'nano .bash_profile' works too, but it's not as easy to edit
-function edit-profile() {
-    cd &&
-    code .bash_profile &&
-    cd -
-}
+alias epro="code ~/.bash_profile"
