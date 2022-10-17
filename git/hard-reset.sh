84827d88a0144f73f17defc63d16faf72cfebaf8
Reid Moffat <18rem8@queensu.ca>
2022-10-17 09:53:57 -0400

    Hard reset already pulls

--- git/hard-reset.sh
+++ git/hard-reset.sh
@@ -5,13 +5,12 @@ function hres() {
     REPO=$(git rev-parse --show-toplevel)
     CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-    read -p "Are you sure you want to perform a HARD RESET (and pull) on '$CURRENT_BRANCH' in '$(basename $REPO)'? (y/n): " -n 1 -r
+    read -p "Are you sure you want to perform a HARD RESET on '$CURRENT_BRANCH' in '$(basename $REPO)'? (y/n): " -n 1 -r
     echo -e "\n"
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        git reset --hard origin/"$CURRENT_BRANCH" &&
-            git pull
+        git reset --hard origin/"$CURRENT_BRANCH"
     else
         echo -e "Cancelling reset..." &&
-            return
+        return
     fi
 }
