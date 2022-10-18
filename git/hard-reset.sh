5b26780c57508ab03103afd15eab95bef85f9484
Reid Moffat <18rem8@queensu.ca>
2022-10-18 14:24:57 -0400

    Hard reset does need a pull
    When I've run it in the past I just didn't need a pull

--- git/hard-reset.sh
+++ git/hard-reset.sh
@@ -5,10 +5,12 @@ function hres() {
     REPO=$(git rev-parse --show-toplevel)
     CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-    read -p "Are you sure you want to perform a HARD RESET on '$CURRENT_BRANCH' in '$(basename $REPO)'? (y/n): " -n 1 -r
+    read -p "Are you sure you want to perform a HARD RESET (and pull) on '$CURRENT_BRANCH' in '$(basename $REPO)'? (y/n): " -n 1 -r
     echo -e "\n"
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        git reset --hard origin/"$CURRENT_BRANCH"
+        git reset --hard origin/"$CURRENT_BRANCH" &&
+        echo -e "\nPulling changes..." &&
+        git pull
     else
         echo -e "Cancelling reset..." &&
         return
