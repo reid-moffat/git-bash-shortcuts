ea204cceee50e42e74a08e9c6008eaf19658b13a
Reid Moffat <18rem8@queensu.ca>
2023-02-04 18:10:47 -0500

    start clarify

--- bash/startup.sh
+++ bash/startup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # It can be a good idea to set your 'username' so it can easily be accessed anywhere in bash and
-# you may have a given username for a job, or just put your name for personal use
+# you may have a given username/ID for a job, or just put your actual name for personal use
 export USER="<your name>"
 echo -e "Welcome back, ${USER}!\n"
 
-# I had an issue wher the shell would start in the root directory an wasn't sure how to change
-# the settings, but a simple few lines can fix that (some terminals have options to do this too)
+# If you want to always start your terminal at a specific path, simply add a cd to the beginning of your profile
+# For example, if you are working with one repository predominantly, you can always start in it
+# The example below is since my terminal would start in the root directory, switching it to the home directory
 if [[ "$PWD" == "/" ]]; then
     cd
 fi
