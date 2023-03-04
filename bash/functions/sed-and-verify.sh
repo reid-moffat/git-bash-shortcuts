049ed354954e7ff41e872d0c1961842f7eaba7a2
Reid Moffat <18rem8@queensu.ca>
2023-03-04 12:14:42 -0500

    Update sed-and-verify.sh

--- bash/functions/sed-and-verify.sh
+++ bash/functions/sed-and-verify.sh
@@ -5,21 +5,20 @@
 #  1) Text to be replaced (properly escaped)
 #  2) Text to replace it with (properly escaped)
 #  3) Path to file to replace
-#  4) How many cases should have been replaced (will)
-#  5) String to print to the console is sucessful (after a check mark)
-#  6) String to print to the console is unsucessful (after a red x)
-# (Params 5 and 6 are optional, but either both of them are present for neither are)
+#  4) How many cases should have been replaced
+#     (will be use for verification; for example in a script to make sure the file you're operating on hasn't changed and broken the script)
 function sed-and-verify() {
-    if [[ "$#" -ne 6 && "$#" -ne 4 ]]; then
-        echo "❌ 4 or 6 parameters are required, $# were provided"
+    if [[ "$#" -ne 4 ]]; then
+        echo "❌ 4 parameters are required, $# were provided"
+        echo "Usage: sed-and-verify [text to replace] [text to replace it with] [file to perform sed on] [expected amount of replacements]"
         return
     fi
 
     # Replace text
     sed -i "s/$1/$2/" $3 &&
     if [[ $(grep -o "$2" $3 | wc -l) == $4 ]]; then
-        echo "✅ Successfully replaced $4 instances of '$1' to '$3' in $4"
+        echo "✅ Successfully replaced $4 instances of '$1' with '$2' in $3"
     else
-        echo "❌ $6"
+        echo "❌ Error replacing '$4' instances of '$1' with '$2' in $3"
     fi
 }
