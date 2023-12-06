f057e5c512422f208807be2df476a33404227a22
Reid Moffat <18rem8@queensu.ca>
2023-12-06 16:51:13 -0500

    Update aliases.sh

--- git/aliases.sh
+++ git/aliases.sh
@@ -7,11 +7,12 @@
 alias gst="git status" # I hate typing the whole thing every time, probabaly the most useful alias since it's used so much
 alias log="git log"
 alias logo="git log --oneline"
+alias branches="git branch -a"
 
 alias dif="git diff"
 alias sdif="git diff --staged" # For already committed files
 
 # Often you'll be working with one, or a couple, of file types; you can easily make a filter to just go through the ones you need
-alias csdif="git diff *.js *.css *.html"
-alias scsdif="git diff --staged *.js *.css *.html"
-alias addcs="git add -u *.js *.css *.html && gst" # -u flag will not add untracked files
+alias fedif="git diff *.js *.css *.html"
+alias sfedif="git diff --staged *.js *.css *.html"
+alias addfe="git add -u *.js *.css *.html && gst" # -u flag will not add untracked files
